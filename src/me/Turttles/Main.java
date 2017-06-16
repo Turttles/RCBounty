46b7efeaf19f87ae7589fef0fac68dd07e75cc4f
Turttles <bradendavis@192.168.0.3>
2017-06-15 23:42:15 -0400

    Added Vault/EssEco Api, added setupEco method

--- src/me/Turttles/Main.java
+++ src/me/Turttles/Main.java
@@ -1,6 +1,50 @@
 package me.Turttles;
 
-public class Main {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.milkbowl.vault.economy.Economy;
+
+public class Main extends JavaPlugin {
+	
+    public static Economy econ = null;
+    public static PluginManager pm = Bukkit.getServer().getPluginManager();
+	
+	public void onEnable() {
+		getLogger().info("[RCB] Loading dependencies...");
+        if (!setupEconomy() ) {
+        	getLogger().severe("No Vault Dependencies found! Disabling... ");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        } else {
+        	getLogger().info("Successfully loaded dependencies..");
+        }
+		
+	}
+	
+	
+	
+	public void onDisable() {
+		
+		
+		
+		
+	}
+	
+	
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
 
 
 
